36dd8f735f2bad104c025723acfcf503aadd32db
Nitin <niting@axioned.com>
2021-10-19 13:24:31 +0530

    js created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
-/* Author: 
+// variable declerations
 
-*/
-
-
+var btns = document.querySelectorAll(".accordion-title");
 
 
-
+btns.forEach(function(val){
+    val.addEventListener("click", function(){
+        val.classList.toggle("active-title");
+        val.nextElementSibling.classList.toggle("active");
+        btns.forEach(function(btn){
+            if(btn != val){
+                btn.classList.remove("active-title");
+                btn.nextElementSibling.classList.remove("active");
+            }
+        });
+    })
+})
 
 
 
